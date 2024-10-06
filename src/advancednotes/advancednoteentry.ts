3e7111a94bedce2770fe1adaa0699ca378c6226e
iLiftALot <nickcorbin17@yahoo.com>
2024-10-06 13:38:39 -0400

    advancednoteentry.ts updates

--- src/advancednotes/advancednoteentry.ts
+++ src/advancednotes/advancednoteentry.ts
@@ -8,35 +8,106 @@ import moment from 'moment';
 export class AdvancedNoteEntry extends NoteEntry {
 	private storageFolder: string;
 	private plugin: ObsidianClipperPlugin;
+	public separator: string;
 
-	constructor(app: App, plugin: ObsidianClipperPlugin, storageFolder: string) {
+	constructor (app: App, plugin: ObsidianClipperPlugin, storageFolder: string) {
 		super(app, false, SectionPosition.APPEND, '');
 		this.storageFolder = storageFolder;
 		this.plugin = plugin;
-		this.template = this.plugin.settings.advancedEntryTemplateLocation;
+		this.separator = `\n\n---\n\n`;
 	}
 
-	async writeToAdvancedNoteStorage(
+	async writeToAdvancedNoteStorage (
 		hostName: string,
 		url: string,
 		title: string,
-		content?: string,
-		comments?: string,
-		description?: string
+		description: string,
+		highlightedContent?: string,
+		comments?: string
 	) {
-		const descriptiveTitle = title;
-		// Remove the prefix from the link, e.g., 'https://wwww.'
-		let baseURI = hostName;
-		const uriHasPrefix = baseURI.match(/^[\w:/]+?\.(?=\S+?\.)/);
-		if (uriHasPrefix) baseURI = baseURI.slice(uriHasPrefix[0].length);
+		// Formatting/processing
+		const uriHasPrefix = hostName.match(/^[\w:/]+?\.(?=\S+?\.)/);
+		const baseURI = uriHasPrefix
+			? hostName.slice(uriHasPrefix[0].length)
+			: hostName;
 		const noteFilePath = `${this.storageFolder}/${baseURI}.md`;
+		const commentData =
+			comments && comments.length > 0 ? `#### Notes\n${comments}` : '';
+		const descriptionData =
+			description && description.length > 0
+				? `#### Description\n${description}`
+				: ``;
+		const titleData = `- [ ] [${title}](${url})`;
+		const formattedHighlightData = highlightedContent
+			? this.getHighlightFormat(highlightedContent)
+			: '';
 
-		const commentData = comments && comments.length > 0 ? `#### Notes\n${comments}` : '';
-		const contentData = content && content.length > 0 ? `#### Highlights\n${content}` : ``;
-		const descriptionData = description && description.length > 0 ? `#### Description\n${description}` : ``;
-		const titleData = `- [ ] [${descriptiveTitle}](${url})`;
-		const sep = `\n\n---\n\n`;
+		// Putting it all together
+		const file = await this.getFile(noteFilePath, baseURI);
+		const supplementalData = `${descriptionData}\n${commentData}\n${formattedHighlightData}`;
+		const { entry, nextFootnoteIndex } = await this.getEntryWithIndex(
+			titleData,
+			url,
+			supplementalData,
+			file
+		);
 
+		await new AppendWriter(this.app, this.openFileOnWrite).write(file, entry);
+
+		return `![[${noteFilePath}#${
+			this.getCurrentDateTime().currentDate
+		}|${baseURI}-${nextFootnoteIndex}]]`;
+	}
+
+	async getEntryWithIndex (
+		title: string,
+		url: string,
+		data: string,
+		file: TFile
+	): Promise<{ entry: string; nextFootnoteIndex: number }> {
+		const { nextFootnoteIndex, sectionHeader } = await this.getReferenceData(
+			file
+		);
+		const entry = `${sectionHeader}\n${title} [^${nextFootnoteIndex}]\n${data}\n\n[^${nextFootnoteIndex}]: ${url}`;
+		return {
+			entry,
+			nextFootnoteIndex,
+		};
+	}
+
+	async getReferenceData (
+		file: TFile
+	): Promise<{ nextFootnoteIndex: number; sectionHeader: string }> {
+		const fileContent = await this.app.vault.read(file);
+		const { currentDate, currentTime } = this.getCurrentDateTime();
+
+		//  Custom regex to detect main date headers of the file
+		const detectDateRegex = new RegExp(`(?<=^## )${currentDate}`, 'gm');
+		const hasDateHeader = fileContent.match(detectDateRegex);
+		let hasOldHeader = false;
+		// Only check if the date header is old if todays date is already in the file
+		if (hasDateHeader) {
+			hasOldHeader = moment(
+				hasDateHeader[0],
+				this.plugin.settings.dateFormat
+			).isBefore(moment(currentDate, this.plugin.settings.dateFormat));
+		}
+
+		// Count the amount of footnotes previously mentioned (referenced by the
+		// specific format of each web-clip header) to obtain the next footnote index
+		const nextFootnoteIndex = (fileContent.match(/^###\s/gm)?.length ?? 0) + 1;
+		// Either include the date header with the timestamped header (if needed), or just the timestamped header
+		const sectionHeader =
+			!hasDateHeader || hasOldHeader
+				? `## ${currentDate}${this.separator}### **${currentTime}**` // create a new section for the new date
+				: `${this.separator}### **${currentTime}**`; // append to the existing date section
+		return {
+			sectionHeader,
+			nextFootnoteIndex,
+		};
+	}
+
+	async getFile (noteFilePath: string, baseURI: string): Promise<TFile> {
 		const folder = this.app.vault.getAbstractFileByPath(this.storageFolder);
 		let file = this.app.vault.getAbstractFileByPath(noteFilePath);
 
@@ -44,9 +115,14 @@ export class AdvancedNoteEntry extends NoteEntry {
 			// create the file and write data
 			if (!(folder instanceof TFolder)) {
 				await this.app.vault.createFolder(this.storageFolder);
-				await new Promise((r) => setTimeout(r, 50));
+				await new Promise(r => setTimeout(r, 50));
 			}
-			file = await this.app.vault.create(noteFilePath, `# ${baseURI}`);
+			const formattedHeaderURI =
+				baseURI.charAt(0).toUpperCase() + baseURI.slice(1);
+			file = await this.app.vault.create(
+				noteFilePath,
+				`# ${formattedHeaderURI}`
+			);
 			if (!file || !(file instanceof TFile)) {
 				const errorMessage = `Unable to create clipper storage file. Most likely ${this.storageFolder} doesn't exist and we were unable to create it.`;
 				console.error(errorMessage);
@@ -54,25 +130,47 @@ export class AdvancedNoteEntry extends NoteEntry {
 				throw Error(errorMessage);
 			}
 		}
-		await new Promise((r) => setTimeout(r, 50));
+		await new Promise(r => setTimeout(r, 50));
+		return file;
+	}
+
+	/**
+	 * @description
+	 * This was created to assist with folding everything together (if you're willing to install the
+	 * `Admonitions` plugin)...
+	 * I felt this was needed because, when you highlight content with various headers, they may over-rule
+	 * the target file's headers and prevent proper folding.
+	 * With or without Admonitions, placing the highlighted content within a quote block allows for complete
+	 * folding regardless of the header levels within the content that was highlighted from the page.
+	 */
+	getHighlightFormat (highlightedContent: string): string {
+		if (!(highlightedContent.length > 0)) {
+			return '';
+		}
+		let newHighlightData = highlightedContent;
+		//newHighlightData = `#### Highlights\n\`\`\`ad-note\ntitle: Highlighted Content\ncollapse: true\n\n${highlightedContent.replaceAll(/```/g, '~~~')}\n\`\`\``;
+		if (this.plugin.hasAdmonitionsPlugin) {
+			// add the highlighted content within an admonition code block
+			newHighlightData = `#### Highlights\n\`\`\`ad-note\ntitle: Highlighted Content\ncollapse: true\n\n${highlightedContent.replaceAll(
+				/```/g,
+				'~~~'
+			)}\n\`\`\``;
+		} else {
+			// add the highlighted content within a regular quote block
+			newHighlightData = `#### Highlights\n> [!info] Highlighted Content\n${highlightedContent
+				.split('\n')
+				.map(line => `> ${line}`)
+				.join('\n')}`;
+		}
+		return newHighlightData;
+	}
+
+	getCurrentDateTime (): { currentDate: string; currentTime: string } {
 		const currentDate = moment().format(this.plugin.settings.dateFormat);
 		const currentTime = moment().format(this.plugin.settings.timestampFormat);
-		const detectDateRegex = new RegExp(`(?<=^## )${currentDate}`, 'gm');
-		const fileContent = await this.app.vault.read(file);
-		const hasDateHeader = fileContent.match(detectDateRegex);
-		let hasOldHeader = false;
-		if (hasDateHeader) {
-			hasOldHeader = moment(hasDateHeader[0], this.plugin.settings.dateFormat)
-				.isBefore(moment(currentDate, this.plugin.settings.dateFormat));
-		}
-		const nextFootnoteIndex = (fileContent.match(/^###\s/gm)?.length ?? 0) + 1;
-		const data = `${descriptionData}\n${commentData}\n${contentData}`;
-		const sectionHeader = !hasDateHeader || hasOldHeader
-			? `## ${currentDate}${sep}### **${currentTime}**`
-			: `${sep}### **${currentTime}**`;
-		const entry = `${sectionHeader}\n${titleData} [^${nextFootnoteIndex}]\n${data}\n\n[^${nextFootnoteIndex}]: ${url}`;
-		await new AppendWriter(this.app, this.openFileOnWrite).write(file, entry);
-
-		return `![[${noteFilePath}#${currentTime}|${baseURI}-${nextFootnoteIndex}]]`;
+		return {
+			currentDate,
+			currentTime,
+		};
 	}
 }
